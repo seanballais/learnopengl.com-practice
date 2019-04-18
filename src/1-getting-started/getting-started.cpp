e3bda8ee0eaf321569ac38bed5d5e7c2f8a5117d
Sean Francis N. Ballais <sean@seanballais.com>
2019-04-18 12:00:25 +0800

    Completed the 'Hello Triangle' unit of the 'Getting Started' chapter.

--- src/1-getting-started/getting-started.cpp
+++ src/1-getting-started/getting-started.cpp
@@ -3,6 +3,24 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+const char *vertexShaderSrc = 
+    "#version 330 core\n"
+    "layout (location = 0) in vec3 aPos;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
+    "}\n";
+
+const char *fragShaderSrc =
+    "#version 330 core\n"
+    "out vec4 FragColor;\n"
+    "\n"
+    "void main()\n"
+    "{\n"
+    "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
+    "}\n";
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow* window);
 
@@ -36,23 +54,110 @@ int main()
     glViewport(0, 0, 800, 600);
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
 
+    // Set up the vertex data.
+    float vertices[] = {
+        0.5f,  0.5f, 0.0f,
+        0.5f, -0.5f, 0.0f,
+        -0.5f, -0.5f, 0.0f,
+        -0.5f,  0.5f, 0.0f
+    };
+    unsigned int indices[] = {
+        0, 1, 3,
+        1, 2, 3
+    };
+
+    // Set up the shaders modern OpenGL requires us to define.
+    char infoLog[512];
+
+    // Set up the vertex shader.
+    unsigned int vertexShader;
+    vertexShader = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vertexShader, 1, &vertexShaderSrc, NULL);
+    glCompileShader(vertexShader);
+
+    int vertexShaderCompileSuccess;
+    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &vertexShaderCompileSuccess);
+    if (!vertexShaderCompileSuccess) {
+        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
+        std::cout << "ERROR::VERTEX_SHADER_COMPILATION_FAILED: " << infoLog << std::endl;
+    }
+
+    // Set up the fragment shader.
+    unsigned int fragShader;
+    fragShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+    glShaderSource(fragShader, 1, &fragShaderSrc, NULL);
+    glCompileShader(fragShader);
+
+    int fragShaderCompileSuccess;
+    glGetShaderiv(fragShader, GL_COMPILE_STATUS, &fragShaderCompileSuccess);
+    if (!fragShaderCompileSuccess) {
+        glGetShaderInfoLog(fragShader, 512, NULL, infoLog);
+        std::cout << "ERROR::FRAGMENT_SHADER_COMPILATION_FAILED: " << infoLog << std::endl;
+    }
+
+    // Let's create the shader program that the shaders will be linked into.
+    unsigned int shaderProgram;
+    shaderProgram = glCreateProgram();
+
+    glAttachShader(shaderProgram, vertexShader);
+    glAttachShader(shaderProgram, fragShader);
+    glLinkProgram(shaderProgram);
+    glDeleteShader(vertexShader);
+    glDeleteShader(fragShader);
+
+    int shaderProgLinkSuccess;
+    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &shaderProgLinkSuccess);
+    if (!shaderProgLinkSuccess) {
+        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
+        std::cout << "ERROR::SHADER_PROGRAM_LINKING_FAILED: " << infoLog << std::endl;
+    }
+
+    // Configure our object for rendering.
+    unsigned int vao;
+    glGenVertexArrays(1, &vao);
+
+    glBindVertexArray(vao);
+
+    unsigned int vbo;
+    glGenBuffers(1, &vbo);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+
+    unsigned int ebo;
+    glGenBuffers(1, &ebo);
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
+    glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*) 0);
+    glEnableVertexAttribArray(0);
+
     // Let's get OpenGL displaying!
     // This is called the render loop.
     while (!glfwWindowShouldClose(window)) {
-        processInput(window);
-
-        // Rendering methods here!
-
-
-        // We should try removing this buffer swapping sometime to see what happens during single buffer drawing.
-        glfwPollEvents();
-
         // We must always call glClear(). Otherwise, we'd be displaying
         // whatever data was in the frame buffer our OpenGL context is using.
         // For funsies, let's clear the frame buffer and paint a pink
         // background.
-        glClearColor(1.0f, 0.70f, 0.70f, 1.0f);
+        glClearColor(0.25f, 0.25f, 0.25f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
+
+        processInput(window);
+
+        // Rendering methods here!
+        glUseProgram(shaderProgram);
+        glBindVertexArray(vao); // For one object, this is unnecessary, but
+                                // we will have more than one object that we
+                                // will render, so calling this will allow our
+                                // code to be forward-looking (despite this
+                                // being a simple addition).
+        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glBindVertexArray(0);
+
+        // We should try removing this buffer swapping sometime to see what happens during single buffer drawing.
+        glfwPollEvents();
+
         glfwSwapBuffers(window);
     }
 
